6cb6beeaed75ed0af34cd331775b016fdfe64331
Tim Schraml <schramltim@gmail.com>
2019-10-05 17:04:36 +0200

    Updates

---
+++ precache-manifest.03fce97dc5afb1e094cd041cf25c1da0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1575bdadb0468b4025bcb12dc144c202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22439c2b2633eb34c09c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a0924bbde606acd0a8c9",
+    "url": "/static/js/2.cfea24b1.chunk.js"
+  },
+  {
+    "revision": "22439c2b2633eb34c09c",
+    "url": "/static/js/main.ba14f20c.chunk.js"
+  },
+  {
+    "revision": "387339a68d9480957460",
+    "url": "/static/js/runtime-main.4c6b0170.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
